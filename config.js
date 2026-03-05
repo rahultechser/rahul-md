bfd551efe7142a322349ecc193b71de794c24e2d
Rahul Debnath <137095636+rahultechser@users.noreply.github.com>
2026-03-05 21:51:16 +0530

    rahultechser

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    ownerName: "Rahul Debnath", // তোমার নাম
+    botName: "Rahul-MD",        // বটের নতুন নাম এখানে সেট করলাম
+    ownerNumber: ["919547261290"], // তোমার নিজের নম্বর এখানে দাও
+    prefix: ".",
+    footer: "Powered by Rahul-MD"
+};
